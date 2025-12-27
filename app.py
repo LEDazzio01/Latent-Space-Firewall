c7c069a56e6000229aa77f8f547b5498c670aea7
L. Elaine Dazzio <170764058+LEDazzio01@users.noreply.github.com>
2025-12-27 21:40:10 +0000

    Add latent-space-firewall to sys.path in app.py to fix module import error

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "latent-space-firewall")))
 import streamlit as st
 import plotly.graph_objects as go
 import time
